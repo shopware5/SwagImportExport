0469cc37aa446b2432f7d868de6f76510267f513
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2014-03-13 13:09:34 +0200

    Init tests

---
+++ Tests/Shopware/ImportExport/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Shopware\ImportExport;
+
+include('./../../../../../../tests/Shopware/TestHelper.php');
+
+Shopware()->Loader()->registerNamespace('Tests\Shopware\ImportExport', __DIR__  . '/');
