4ebbbc8b47d34f4f2b017a882a3d25c03796196e
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2014-03-13 10:19:57 +0200

    Init

---
+++ Bootstrap.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Shopware 4.2
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * Shopware SwagImportExport Plugin - Bootstrap
+ *
+ * @category  Shopware
+ * @package   Shopware\Components\Console\Command
+ * @copyright Copyright (c) 2014, shopware AG (http://www.shopware.de)
+ */
+class Shopware_Plugins_Backend_SwagImportExport_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+
+    /**
+     * Returns the plugin label which is displayed in the plugin information and
+     * in the Plugin Manager.
+     * @return string
+     */
+    public function getLabel()
+    {
+        return 'Swag Import/Export';
+    }
+
+    /**
+     * Returns the current version of the plugin.
+     * @return string
+     */
+    public function getVersion()
+    {
+        return "1.0.0";
+    }
+
+    /**
+     * Install function of the plugin bootstrap.
+     *
+     * Registers all necessary components and dependencies.
+     *
+     * @return bool
+     */
+    public function install()
+    {
+
+        return true;
+    }
+
+    /**
+     * Uninstall function of the plugin.
+     * Fired from the plugin manager.
+     * @return bool
+     */
+    public function uninstall()
+    {
+
+        return true;
+    }
+
+}
