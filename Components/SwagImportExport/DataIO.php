a2fa9116298f1e31b22a347032d3d9b8afd1ebd8
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2014-04-04 16:32:25 +0300

    Added progress, close and resume session.

--- Components/SwagImportExport/DataIO.php
+++ Components/SwagImportExport/DataIO.php
@@ -31,14 +31,14 @@ class DataIO
      * @var array
      */
     private $recordIds;
-    
+
     /**
      * Type of the dataIO - export/import
      * 
      * @var string
      */
     private $type;
-    
+
     /**
      * Format of the doc - csv, xml
      * 
@@ -74,15 +74,15 @@ class DataIO
      */
     public function read($records)
     {
-        //todo: get from where to start 
-        $start = 0;
+        $start = $this->dataSession->getPosition();
+
         $ids = $this->loadIds($start, $records);
 
         $columns = $this->getColumns();
 
         $dbAdapter = $this->getDbAdapter();
         $rawData = $dbAdapter->read($ids, $columns);
-        
+
         return $rawData;
     }
 
@@ -93,7 +93,7 @@ class DataIO
     public function preloadRecordIds()
     {
         //todo: check from ExportSession        
-        
+
         $dbAdapter = $this->getDbAdapter();
         $limitAdapater = $this->getLimitAdapter();
         $filterAdapter = $this->getFilterAdapter();
@@ -101,12 +101,12 @@ class DataIO
         $ids = $dbAdapter->readRecordIds(
                 $limitAdapater->getOffset(), $limitAdapater->getLimit(), $filterAdapter->getFilter()
         );
-        
+
         $this->setRecordIds($ids);
-        
+
         return $this;
     }
-    
+
     /**
      * Returns the state of the session. 
      * active: 
@@ -125,53 +125,113 @@ class DataIO
     {
         return $this->dataSession->getState();
     }
-    
+
     /**
      * Check if the session contains ids.
      * If the session has no ids, then the db adapter must be used to retrieve them.
      * Then writes these ids to the session and sets the session state to "active".
      * For now we will write the ids as a serialized array.
      */
-    public function startSession() 
+    public function startSession()
     {
         $ids = $this->preloadRecordIds()->getRecordIds();
         $type = $this->getType();
-        
+
         $session = $this->getDataSession();
-        
+
         //set type
         $session->setType($type);
-        
+
         //set ids
         $session->setIds(serialize($ids));
-        
+
         //set position
         $session->setPosition(0);
-        
+
         //set count
         $session->setCount(count($ids));
-        
+
         $dateTime = new \DateTime('now');
-        
+
         //set date/time
         $session->setCreatedAt($dateTime);
-               
+
         $fileName = $type . '-' . $dateTime->format('Y-m-d_H-i-s');
-        
+
         //set count
         $session->setFileName($fileName);
-        
+
         //set format
         $session->setFormat($this->getFormat());
-        
+
         //change state
         $session->setState('active');
-        
+
         Shopware()->Models()->persist($session);
-            
+
         Shopware()->Models()->flush();
     }
-    
+
+    /**
+     * Checks if the number of processed records has reached the current max records count.
+     * If reached then the session state will be set to "stopped"
+     * Updates the session position with the current position (stored in a member variable).
+     *
+     */
+    public function progressSession()
+    {
+        //todo: get step ???
+        $step = 100;
+
+        $session = $this->getDataSession();
+
+        $position = $session->getPosition();
+        $count = $session->getCount();
+
+        $newPosition = $position + $step;
+
+        if ($newPosition >= $count) {
+            $session->setState('finished');
+            $session->setPosition($count);
+        } else {
+            $session->setPosition($newPosition);
+        }
+
+        Shopware()->Models()->persist($session);
+
+        Shopware()->Models()->flush();
+    }
+
+    /**
+     * Marks the session as closed (sets the session state as "closed").
+     * If the session progress has not reached to the end, throws an exception.
+     */
+    public function closeSession()
+    {
+        $session = $this->getDataSession();
+        $session->setState('closed');
+
+        Shopware()->Models()->persist($session);
+
+        Shopware()->Models()->flush();
+    }
+
+    /**
+     * Checks also the current position - if all the ids of the session are done, then the function does nothing.
+     * Otherwise it sets the session state from "suspended" to "active", so that it is ready again for processing.
+     */
+    public function resumeSession()
+    {
+        //todo: maybe check state before make it active ???
+        $session = $this->getDataSession();
+
+        $session->setState('active');
+
+        Shopware()->Models()->persist($session);
+
+        Shopware()->Models()->flush();
+    }
+
     /**
      * Returns the max records count initialized in the constructor.
      */
@@ -209,22 +269,22 @@ class DataIO
     {
         $this->recordIds = $recordIds;
     }
-    
+
     public function getDataSession()
     {
         return $this->dataSession;
     }
-    
+
     public function getType()
     {
         return $this->type;
     }
-    
+
     public function getFormat()
     {
         return $this->format;
     }
-    
+
     /**
      * Returns db columns
      * 
@@ -233,11 +293,11 @@ class DataIO
     public function getColumns()
     {
         $colOptions = $this->getColumnOptionsAdapter()->getColumnOptions();
-        
-        if ($colOptions === null || empty($colOptions)){
+
+        if ($colOptions === null || empty($colOptions)) {
             $colOptions = $this->getDbAdapter()->getDefaultColumns();
         }
-        
+
         return $colOptions;
     }
 
@@ -257,11 +317,13 @@ class DataIO
             throw new \Exception('No loaded records ids');
         }
 
+        $end = $start + $records;
+
         $filterIds = array();
         $counter = 0;
 
         foreach ($storedIds as $index => $id) {
-            if ($index >= $start && $counter < $records) {
+            if ($index >= $start && $counter < $end) {
                 $filterIds[] = $id;
                 $counter ++;
             }
