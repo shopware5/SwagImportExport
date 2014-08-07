1d55f5d25f1cbdd870a762aa9207ea67eda41d8a
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2014-08-07 11:17:18 +0300

    Removed default filter and offset values

--- Components/SwagImportExport/Utils/DataLimit.php
+++ Components/SwagImportExport/Utils/DataLimit.php
@@ -13,14 +13,11 @@ class DataLimit
     {
         if (isset($options['limit'])) {
             $this->limit = $options['limit'];
-        } else {
-            $this->limit = 0;
-        }
+        } 
+        
         if (isset($options['offset'])) {
             $this->offset = $options['offset'];
-        } else {
-            $this->offset = 0;
-        }
+        } 
     }
 
     public function getLimit()
