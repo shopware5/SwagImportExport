987619b214d31d006f4936e845f2de7d57a80d36
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2014-08-28 16:55:39 +0300

    Added import to article image db adapter

--- Components/SwagImportExport/DbAdapters/ArticlesImagesDbAdapter.php
+++ Components/SwagImportExport/DbAdapters/ArticlesImagesDbAdapter.php
@@ -9,6 +9,7 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
      * Shopware\Components\Model\ModelManager
      */
     protected $manager;
+    protected $articleDetailRepository;
 
     /**
      * Returns record ids
@@ -29,19 +30,19 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
         $builder->from('Shopware\Models\Article\Image', 'image')
                 ->where('image.articleDetailId IS NULL')
                 ->andWhere('image.parentId IS NULL');
-        
+
         $builder->setFirstResult($start)
                 ->setMaxResults($limit);
 
         $records = $builder->getQuery()->getResult();
-        
+
         $result = array();
         if ($records) {
             foreach ($records as $value) {
                 $result[] = $value['id'];
             }
         }
-        
+
         return $result;
     }
 
@@ -67,13 +68,15 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
         $builder = $manager->createQueryBuilder();
         $builder->select($columns)
                 ->from('Shopware\Models\Article\Image', 'aimage')
-                ->leftJoin('aimage.article', 'article')
-                ->leftJoin('aimage.articleDetail', 'articleDetail')
+                ->join('aimage.article', 'article')
+                ->join('article.details', 'articleDetail')
                 ->where('aimage.id IN (:ids)')
+                ->andWhere('articleDetail.kind = 1')
+//                ->groupBy('aimage.id')
                 ->setParameter('ids', $ids);
 
         $result['default'] = $builder->getQuery()->getResult();
-        
+
         return $result;
     }
 
@@ -85,10 +88,10 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
     public function getDefaultColumns()
     {
         $request = Shopware()->Front()->Request();
-        $path = $request->getScheme().'://'.$request->getHttpHost().$request->getBasePath().'/media/image/';
-        
+        $path = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/media/image/';
+
         $columns = array(
-            'articleDetail.number as orderNumber',
+            'articleDetail.number as ordernumber',
             "CONCAT('$path', aimage.path, '.', aimage.extension) as image",
             'aimage.main as main',
             'aimage.description as description',
@@ -107,13 +110,76 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
      */
     public function write($records)
     {
-//        $manager = $this->getManager();
-//        
-//        foreach ($records['default'] as $record) {
-//            
-//        }
+        $db = Shopware()->Db();
+
+        foreach ($records['default'] as $record) {
+            if (empty($record['ordernumber']) || empty($record['image'])) {
+                throw new \Exception('Ordernumber and image are required');
+            }
+
+            /** @var \Shopware\Models\Article\Detail $articleDetailModel */
+            $articleDetailModel = $this->getArticleDetailRepository()->findOneBy(array('number' => $record['ordernumber']));
+            if (!$articleDetailModel) {
+                throw new \Exception(sprintf('Article with number %s does not exists', $record['ordernumber']));
+            }
+
+            /** @var \Shopware\Models\Article\Article $article */
+            $article = $articleDetailModel->getArticle();
+
+            $name = pathinfo($record['image'], PATHINFO_FILENAME);
+            $path = $this->load($record['image'], $name);
+
+            $file = new \Symfony\Component\HttpFoundation\File\File($path);
+
+            $media = new \Shopware\Models\Media\Media();
+            $media->setAlbumId(-1);
+            $media->setAlbum($this->getManager()->find('Shopware\Models\Media\Album', -1));
+
+            $media->setFile($file);
+            $media->setName(pathinfo($record['image'], PATHINFO_FILENAME));
+            $media->setDescription('');
+            $media->setCreated(new \DateTime());
+            $media->setUserId(0);
+
+            $this->getManager()->persist($media);
+            $this->getManager()->flush();
+
+            if (empty($record['main'])) {
+                $record['main'] = 1;
+            }
+
+            //generate thumbnails
+            if ($media->getType() == \Shopware\Models\Media\Media::TYPE_IMAGE) {
+                /*                 * @var $manager \Shopware\Components\Thumbnail\Manager */
+                $manager = Shopware()->Container()->get('thumbnail_manager');
+                $manager->createMediaThumbnail($media, array(), true);
+            }
+
+            $image = new \Shopware\Models\Article\Image();
+            $image->setArticle($article);
+            $image->setDescription($record['description']);
+            $image->setPosition($record['position']);
+            $image->setPath($media->getName());
+            $image->setExtension($media->getExtension());
+            $image->setMedia($media);
+            $image->setMain($record['main']);
+
+            $this->getManager()->persist($image);
+            $this->getManager()->flush($image);
+
+            // Prevent multiple images from being a preview
+            if ((int) $record['main'] === 1) {
+                $db->update('s_articles_img', 
+                        array('main' => 2), 
+                        array(
+                            'articleID = ?' => $article->getId(),
+                            'id <> ?' => $image->getId()
+                        )
+                );
+            }
+        }
     }
-    
+
     /**
      * @return array
      */
@@ -123,7 +189,7 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
             array('id' => 'default', 'name' => 'default')
         );
     }
-    
+
     /**
      * @param string $section
      * @return mix
@@ -131,14 +197,14 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
     public function getColumns($section)
     {
         $method = 'get' . ucfirst($section) . 'Columns';
-        
+
         if (method_exists($this, $method)) {
             return $this->{$method}();
         }
 
         return false;
     }
-    
+
     /**
      * Returns entity manager
      * 
@@ -151,6 +217,87 @@ class ArticlesImagesDbAdapter implements DataDbAdapter
         }
 
         return $this->manager;
+    }
+
+    /**
+     * Helper function to get access to the articleDetail repository.
+     * @return \Shopware\Components\Model\ModelRepository
+     */
+    public function getArticleDetailRepository()
+    {
+        if ($this->articleDetailRepository === null) {
+            $this->articleDetailRepository = $this->getManager()->getRepository('Shopware\Models\Article\Detail');
+        }
+        return $this->articleDetailRepository;
+    }
+
+    /**
+     * @param string $url URL of the resource that should be loaded (ftp, http, file)
+     * @param string $baseFilename Optional: Instead of creating a hash, create a filename based on the given one
+     * @return bool|string returns the absolute path of the downloaded file
+     * @throws \InvalidArgumentException
+     * @throws \Exception
+     */
+    protected function load($url, $baseFilename = null)
+    {
+        $destPath = Shopware()->DocPath('media_' . 'temp');
+        if (!is_dir($destPath)) {
+            mkdir($destPath, 0777, true);
+        }
+
+        $destPath = realpath($destPath);
+
+        if (!file_exists($destPath)) {
+            throw new \InvalidArgumentException(
+            sprintf("Destination directory '%s' does not exist.", $destPath)
+            );
+        } elseif (!is_writable($destPath)) {
+            throw new \InvalidArgumentException(
+            sprintf("Destination directory '%s' does not have write permissions.", $destPath)
+            );
+        }
+
+        $urlArray = parse_url($url);
+        $urlArray['path'] = explode("/", $urlArray['path']);
+        switch ($urlArray['scheme']) {
+            case "ftp":
+            case "http":
+            case "https":
+            case "file":
+                $counter = 1;
+                if ($baseFilename === null) {
+                    $filename = md5(uniqid(rand(), true));
+                } else {
+                    $filename = $baseFilename;
+                }
+
+                while (file_exists("$destPath/$filename")) {
+                    if ($baseFilename) {
+                        $filename = "$counter-$baseFilename";
+                        $counter++;
+                    } else {
+                        $filename = md5(uniqid(rand(), true));
+                    }
+                }
+
+                if (!$put_handle = fopen("$destPath/$filename", "w+")) {
+                    throw new \Exception("Could not open $destPath/$filename for writing");
+                }
+
+                if (!$get_handle = fopen($url, "r")) {
+                    throw new \Exception("Could not open $url for reading");
+                }
+                while (!feof($get_handle)) {
+                    fwrite($put_handle, fgets($get_handle, 4096));
+                }
+                fclose($get_handle);
+                fclose($put_handle);
+
+                return "$destPath/$filename";
+        }
+        throw new \InvalidArgumentException(
+        sprintf("Unsupported schema '%s'.", $urlArray['scheme'])
+        );
     }
 
 }
