02a15307e2357d7db20188ceff37af214c981b5b
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2015-04-17 10:29:04 +0300

    PT-2850 Added import functionality to translation adapter

--- Components/SwagImportExport/DbAdapters/TranslationsDbAdapter.php
+++ Components/SwagImportExport/DbAdapters/TranslationsDbAdapter.php
@@ -2,13 +2,22 @@
 
 namespace Shopware\Components\SwagImportExport\DbAdapters;
 
-use \Shopware\Components\SwagImportExport\Utils\SnippetsHelper as SnippetsHelper;
+use Shopware\Components\SwagImportExport\Utils\SnippetsHelper as SnippetsHelper;
 use Shopware\Components\SwagImportExport\Exception\AdapterException;
 
 class TranslationsDbAdapter implements DataDbAdapter
 {
 
     protected $manager;
+
+    protected $configuratorGroupRepo;
+    protected $configuratorOptionRepo;
+
+    protected $propertyGroupRepo;
+    protected $propertyOptionRepo;
+    protected $propertyValueRepo;
+
+    protected $logMessages;
 
     /**
      * @param $start
@@ -68,13 +77,7 @@ class TranslationsDbAdapter implements DataDbAdapter
 
     public function prepareTranslations($translations)
     {
-        $mapper = array(
-            'configuratorgroup' => 'name',
-            'configuratoroption' => 'name',
-            'propertygroup' => 'groupName',
-            'propertyoption' => 'optionName',
-            'propertyvalue' => 'optionValue',
-        );
+        $mapper = $this->getElemenetMapper();
 
         $result = array();
         foreach ($translations as $index => $translation){
@@ -139,23 +142,122 @@ class TranslationsDbAdapter implements DataDbAdapter
 
     public function write($records)
     {
-        // TODO: Implement write() method.
+        if ($records['default'] == null) {
+            return;
+        }
+
+        $records = Shopware()->Events()->filter(
+            'Shopware_Components_SwagImportExport_DbAdapters_TranslationsDbAdapter_Write',
+            $records,
+            array('subject' => $this)
+        );
+
+        $importMapper = $this->getElemenetMapper();
+
+        $translationWriter = new \Shopware_Components_Translation();
+
+        foreach ($records['default'] as $index => $record) {
+            try {
+
+                if (!isset($record['objectType']) || empty($record['objectType'])) {
+                    $message = SnippetsHelper::getNamespace()
+                        ->get('adapters/translations/object_type_not_found', 'Object type is required.');
+                    throw new AdapterException($message);
+                }
+
+                if (isset($record['languageId'])) {
+                    $shop = $this->getManager()->find('Shopware\Models\Shop\Shop', $record['languageId']);
+                }
+
+                if (!$shop) {
+                    $message = SnippetsHelper::getNamespace()
+                        ->get('adapters/translations/lang_id_not_found', 'Language with id %s does not exists');
+                    throw new \AdapterException(sprintf($message, $record['languageId']));
+                }
+
+                $repository = $this->getRepository($record['objectType']);
+
+                if (!isset($record['objectKey']) && !empty($record['objectKey'])){
+                    $element = $repository->findOneBy(array('id' => (int) $record['objectKey']));
+
+                    if(!$element){
+                        $message = SnippetsHelper::getNamespace()
+                            ->get('adapters/translations/element_id_not_found', '%s element not found with ID %s');
+                        throw new AdapterException(sprintf($message, $record['objectType'], $record['objectKey']));
+                    }
+                } else if (isset($record['baseName']) && !empty($record['baseName'])){
+                    $findKey = $record['objectType'] === 'propertyvalue' ? 'value': 'name';
+                    $element = $repository->findOneBy(array($findKey => $record['baseName']));
+
+                    if(!$element){
+                        $message = SnippetsHelper::getNamespace()
+                            ->get('adapters/translations/element_baseName_not_found', '%s element not found with name %s');
+                        throw new AdapterException(sprintf($message, $record['objectType'], $record['baseName']));
+                    }
+                }
+
+                if (!$element){
+                    $message = SnippetsHelper::getNamespace()
+                        ->get('adapters/translations/element_objectKey_baseName_not_found', 'Please provide objectKey or baseName');
+                    throw new AdapterException(sprintf($message));
+                }
+
+                if (!isset($record['name'])){
+                    $message = SnippetsHelper::getNamespace()
+                        ->get('adapters/translations/element_name_not_found', 'Please provide name');
+                    throw new AdapterException(sprintf($message));
+                }
+
+                $key = $importMapper[$record['objectType']];
+                $data[$key] = $record['name'];
+
+                if($record['objectType'] == 'configuratorgroup'){
+                    $data['description'] = $record['description'];
+                }
+
+                $translationWriter->write($shop->getId(), $record['objectType'], $element->getId(), $data);
+
+                unset($shop);
+                unset($element);
+                unset($data);
+
+            } catch (AdapterException $e) {
+                $message = $e->getMessage();
+                $this->saveMessage($message);
+            }
+        }
     }
 
     public function getUnprocessedData()
     {
-        // TODO: Implement getUnprocessedData() method.
+
+    }
+
+    public function saveMessage($message)
+    {
+        $errorMode = Shopware()->Config()->get('SwagImportExportErrorMode');
+
+        if ($errorMode === false) {
+            throw new \Exception($message);
+        }
+
+        $this->setLogMessages($message);
     }
 
     public function getLogMessages()
     {
-        // TODO: Implement getLogMessages() method.
+        return $this->logMessages;
+    }
+
+    public function setLogMessages($logMessages)
+    {
+        $this->logMessages[] = $logMessages;
     }
 
     /**
      * Returns entity manager
      *
-     * @return Shopware\Components\Model\ModelManager
+     * @return \Shopware\Components\Model\ModelManager
      */
     public function getManager()
     {
@@ -177,12 +279,23 @@ class TranslationsDbAdapter implements DataDbAdapter
         return $builder;
     }
 
+    protected function getElemenetMapper()
+    {
+        return array(
+            'configuratorgroup' => 'name',
+            'configuratoroption' => 'name',
+            'propertygroup' => 'groupName',
+            'propertyoption' => 'optionName',
+            'propertyvalue' => 'optionValue',
+        );
+    }
+
     /**
      * @param $ids
      * @return array
      * @throws \Zend_Db_Statement_Exception
      */
-    public function getTranslations($ids)
+    protected function getTranslations($ids)
     {
         $articleDetailIds = implode(',', $ids);
 
@@ -238,5 +351,96 @@ class TranslationsDbAdapter implements DataDbAdapter
                 ";
 
         return Shopware()->Db()->query($sql)->fetchAll();
+    }
+
+    protected function getRepository($type)
+    {
+        switch ($type) {
+            case 'configuratorgroup':
+                return $this->getConfiguratorGroupRepository();
+            case 'configuratoroption':
+                return $this->getConfiguratorOptionRepository();
+            case 'propertygroup':
+                return $this->getPropertyGroupRepository();
+            case 'propertyoption':
+                return $this->getPropertyOptionRepository();
+            case 'propertyvalue':
+                return $this->getPropertyValueRepository();
+            default:
+                $message = SnippetsHelper::getNamespace()
+                    ->get('adapters/translations/object_type_not_existing', 'Object type %s not existing.');
+                throw new AdapterException(sprintf($message, $type));
+
+        }
+    }
+
+    /**
+     * Returns configurator group repository
+     *
+     * @return \Shopware\Models\Article\Configurator\Group
+     */
+    public function getConfiguratorGroupRepository()
+    {
+        if ($this->configuratorGroupRepo === null) {
+            $this->configuratorGroupRepo = $this->getManager()->getRepository('Shopware\Models\Article\Configurator\Group');
+        }
+
+        return $this->configuratorGroupRepo;
+    }
+
+    /**
+     * Returns configurator option repository
+     *
+     * @return \Shopware\Models\Article\Configurator\Option
+     */
+    public function getConfiguratorOptionRepository()
+    {
+        if ($this->configuratorOptionRepo === null) {
+            $this->configuratorOptionRepo = $this->getManager()->getRepository('Shopware\Models\Article\Configurator\Option');
+        }
+
+        return $this->configuratorOptionRepo;
+    }
+
+    /**
+     * Returns property group repository
+     *
+     * @return \Shopware\Models\Property\Group
+     */
+    public function getPropertyGroupRepository()
+    {
+        if ($this->propertyGroupRepo === null) {
+            $this->propertyGroupRepo = $this->getManager()->getRepository('Shopware\Models\Property\Group');
+        }
+
+        return $this->propertyGroupRepo;
+    }
+
+    /**
+     * Returns property option repository
+     *
+     * @return \Shopware\Models\Property\Option
+     */
+    public function getPropertyOptionRepository()
+    {
+        if ($this->propertyOptionRepo === null) {
+            $this->propertyOptionRepo = $this->getManager()->getRepository('Shopware\Models\Property\Option');
+        }
+
+        return $this->propertyOptionRepo;
+    }
+
+    /**
+     * Returns property value repository
+     *
+     * @return \Shopware\Models\Property\Value
+     */
+    public function getPropertyValueRepository()
+    {
+        if ($this->propertyValueRepo === null) {
+            $this->propertyValueRepo = $this->getManager()->getRepository('Shopware\Models\Property\Value');
+        }
+
+        return $this->propertyValueRepo;
     }
 }
