0ef1c8715998c6831d12c42de99c4482e2b1616f
Anelia Lambreba <anelia.lambreva@dnhsoft.com>
2015-09-01 11:24:49 +0300

    PT-2436 articles in stock adapter - check required fields;

---
+++ Components/SwagImportExport/Validators/ArticleInStockValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Shopware\Components\SwagImportExport\Validators;
+
+use Shopware\Components\SwagImportExport\Utils\SnippetsHelper;
+use Shopware\Components\SwagImportExport\Exception\AdapterException;
+
+class ArticleInStockValidator extends Validator
+{
+    private $requiredFields = array(
+        'orderNumber',
+    );
+
+    private $snippetData = array(
+        'orderNumber' => array(
+            'adapters/ordernumber_required',
+            'Order number is required'
+        ),
+    );
+
+    public function checkRequiredFields($record)
+    {
+        foreach ($this->requiredFields as $key) {
+            if (isset($record[$key])) {
+                continue;
+            }
+
+            list($snippetName, $snippetMessage) = $this->snippetData[$key];
+
+            $message = SnippetsHelper::getNamespace()->get($snippetName, $snippetMessage);
+            throw new AdapterException($message);
+        }
+    }
+}
