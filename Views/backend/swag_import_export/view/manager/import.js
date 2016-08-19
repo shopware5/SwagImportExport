5ea96162cc0b13cf91eb9eed5ac6279573313a1e
Patrick Stahl <P.Stahl@shopware.com>
2016-08-19 15:32:36 +0200

    PT-6392 - Add paging to profile-dropdown

--- Views/backend/swag_import_export/view/manager/import.js
+++ Views/backend/swag_import_export/view/manager/import.js
@@ -1,26 +1,4 @@
 /**
- * Shopware 4
- * Copyright © shopware AG
- *
- * According to our dual licensing model, this program can be used either
- * under the terms of the GNU Affero General Public License, version 3,
- * or under a proprietary license.
- *
- * The texts of the GNU Affero General Public License with an additional
- * permission and of our proprietary license can be found at and
- * in the LICENSE file you have received along with this program.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * "Shopware" is a registered trademark of shopware AG.
- * The licensing of the program under the AGPLv3 does not imply a
- * trademark license. Therefore any rights, title and interest in
- * our trademarks remain entirely with us.
- */
-/**
  * Shopware SwagImportExport Plugin
  *
  * @category Shopware
@@ -214,7 +192,7 @@ Ext.define('Shopware.apps.SwagImportExport.view.manager.Import', {
     createProfileCombo: function() {
         var me = this;
 
-        me.profileCombo = Ext.create('Ext.form.field.ComboBox', {
+        me.profileCombo = Ext.create('Shopware.form.field.PagingComboBox', {
             fieldLabel: me.snippets.selectProfile,
             store: me.profilesStore,
             labelStyle: 'font-weight: 700; text-align: left;',
