da6f4deb1348b69ff79ef24d3dcfc5bccdd693da
Simon Bäumer <s.baeumer@shopware.com>
2016-09-14 13:44:00 +0200

    PT-6700 - Implement import cli command tests

--- Tests/Helper/CommandTestHelper.php
+++ Tests/Helper/CommandTestHelper.php
@@ -24,38 +24,49 @@ class CommandTestHelper
      */
     private $createdFiles = [];
 
-    const ARTICLE_PROFILE = 'articles';
+    const IMPORT_FILES_DIR = __DIR__ . '/ImportFiles/';
+
+    const ARTICLE_PROFILE_TYPE = 'articles';
     const ARTICLE_PROFILE_NAME = 'article_profile';
+    const ARTICLE_TABLE = 's_articles';
 
-    const CUSTOMER_PROFILE = 'customers';
+    const VARIANT_PROFILE_TYPE = 'articles';
+    const VARIANT_PROFILE_NAME = 'variant_profile';
+    const VARIANT_TABLE = 's_articles_details';
+
+    const CUSTOMER_PROFILE_TYPE = 'customers';
     const CUSTOMER_PROFILE_NAME = 'customer_profile';
+    const CUSTOMER_TABLE = 's_user';
 
-    const CATEGORY_PROFILE = 'categories';
+    const CATEGORY_PROFILE_TYPE = 'categories';
     const CATEGORY_PROFILE_NAME = 'category_profile';
+    const CATEGORY_TABLE = 's_categories';
 
-    const ARTICLES_INSTOCK_PROFILE = 'articlesInStock';
+    const ARTICLES_INSTOCK_PROFILE_TYPE = 'articlesInStock';
     const ARTICLES_INSTOCK_PROFILE_NAME = 'article_instock_profile';
 
-    const ARTICLES_PRICES_PROFILE = 'articlesPrices';
+    const ARTICLES_PRICES_PROFILE_TYPE = 'articlesPrices';
     const ARTICLES_PRICES_PROFILE_NAME = 'articles_price_profile';
 
-    const ARTICLES_IMAGES_PROFILE = 'articlesImages';
+    const ARTICLES_IMAGES_PROFILE_TYPE = 'articlesImages';
     const ARTICLES_IMAGE_PROFILE_NAME = 'articles_image_profile';
 
-    const ARTICLES_TRANSLATIONS_PROFILE = 'articlesTranslations';
-    const ARTICLES_TRANSLATIONS_PROFILE_NAME = 'articles_translations_name';
+    const ARTICLES_TRANSLATIONS_PROFILE_TYPE = 'articlesTranslations';
+    const ARTICLES_TRANSLATIONS_PROFILE_NAME = 'articles_translations_profile';
 
-    const ORDERS_PROFILE = 'orders';
+    const ORDERS_PROFILE_TYPE = 'orders';
     const ORDERS_PROFILE_NAME = 'order_profile';
 
-    const MAIN_ORDERS_PROFILE = 'mainOrders';
+    const MAIN_ORDERS_PROFILE_TYPE = 'mainOrders';
     const MAIN_ORDERS_PROFILE_NAME = 'main_order_profile';
+    const IMPORT_MAIN_ORDER_PROFILE_NAME = 'order_profile';
 
-    const TRANSLATIONS_PROFILE = 'translations';
+    const TRANSLATIONS_PROFILE_TYPE = 'translations';
     const TRANSLATIONS_PROFILE_NAME = 'translation_profile';
 
-    const NEWSLETTER_PROFILE = 'newsletter';
+    const NEWSLETTER_PROFILE_TYPE = 'newsletter';
     const NEWSLETTER_PROFILE_NAME = 'newsletter_profile';
+    const NEWSLETTER_TABLE = 's_campaigns_mailaddresses';
 
     /**
      * @var ModelManager
@@ -136,17 +147,18 @@ class CommandTestHelper
 
     private function createProfiles()
     {
-        $this->createProfile(self::ARTICLE_PROFILE, self::ARTICLE_PROFILE_NAME);
-        $this->createProfile(self::CUSTOMER_PROFILE, self::CUSTOMER_PROFILE_NAME);
-        $this->createProfile(self::CATEGORY_PROFILE, self::CATEGORY_PROFILE_NAME);
-        $this->createProfile(self::ARTICLES_INSTOCK_PROFILE, self::ARTICLES_INSTOCK_PROFILE_NAME);
-        $this->createProfile(self::ARTICLES_PRICES_PROFILE, self::ARTICLES_PRICES_PROFILE_NAME);
-        $this->createProfile(self::ARTICLES_IMAGES_PROFILE, self::ARTICLES_IMAGE_PROFILE_NAME);
-        $this->createProfile(self::ARTICLES_TRANSLATIONS_PROFILE, self::ARTICLES_TRANSLATIONS_PROFILE_NAME);
-        $this->createProfile(self::ORDERS_PROFILE, self::ORDERS_PROFILE_NAME);
-        $this->createProfile(self::MAIN_ORDERS_PROFILE, self::MAIN_ORDERS_PROFILE_NAME);
-        $this->createProfile(self::TRANSLATIONS_PROFILE, self::TRANSLATIONS_PROFILE_NAME);
-        $this->createProfile(self::NEWSLETTER_PROFILE, self::NEWSLETTER_PROFILE_NAME);
+        $this->createProfile(self::ARTICLE_PROFILE_TYPE, self::ARTICLE_PROFILE_NAME);
+        $this->createProfile(self::VARIANT_PROFILE_TYPE, self::VARIANT_PROFILE_NAME);
+        $this->createProfile(self::CUSTOMER_PROFILE_TYPE, self::CUSTOMER_PROFILE_NAME);
+        $this->createProfile(self::CATEGORY_PROFILE_TYPE, self::CATEGORY_PROFILE_NAME);
+        $this->createProfile(self::ARTICLES_INSTOCK_PROFILE_TYPE, self::ARTICLES_INSTOCK_PROFILE_NAME);
+        $this->createProfile(self::ARTICLES_PRICES_PROFILE_TYPE, self::ARTICLES_PRICES_PROFILE_NAME);
+        $this->createProfile(self::ARTICLES_IMAGES_PROFILE_TYPE, self::ARTICLES_IMAGE_PROFILE_NAME);
+        $this->createProfile(self::ARTICLES_TRANSLATIONS_PROFILE_TYPE, self::ARTICLES_TRANSLATIONS_PROFILE_NAME);
+        $this->createProfile(self::ORDERS_PROFILE_TYPE, self::ORDERS_PROFILE_NAME);
+        $this->createProfile(self::MAIN_ORDERS_PROFILE_TYPE, self::MAIN_ORDERS_PROFILE_NAME);
+        $this->createProfile(self::TRANSLATIONS_PROFILE_TYPE, self::TRANSLATIONS_PROFILE_NAME);
+        $this->createProfile(self::NEWSLETTER_PROFILE_TYPE, self::NEWSLETTER_PROFILE_NAME);
 
         $this->modelManager->flush();
     }
