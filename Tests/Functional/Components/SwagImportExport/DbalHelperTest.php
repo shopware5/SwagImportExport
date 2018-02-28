5b415ea669f882e7a4248a61501e446f9ce954bb
Michael Telgmann <m.telgmann@shopware.com>
2018-02-28 14:02:34 +0100

    PT-8329 - Improve code and fix code style

--- Tests/Functional/Components/SwagImportExport/DbalHelperTest.php
+++ Tests/Functional/Components/SwagImportExport/DbalHelperTest.php
@@ -9,10 +9,12 @@
 namespace Functional\Components\SwagImportExport;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
+use PHPUnit\Framework\TestCase;
 use Shopware\Components\Model\ModelManager;
 use Shopware\Components\SwagImportExport\DbalHelper;
+use Shopware\Models\Article\Article;
 
-class DbalHelperTest extends \PHPUnit_Framework_TestCase
+class DbalHelperTest extends TestCase
 {
     const EXAMPLE_TABLE = 'example_table';
 
@@ -47,8 +49,9 @@ class DbalHelperTest extends \PHPUnit_Framework_TestCase
 
         $modelManagerStub = $this->getModelManagerStub();
         $connection = Shopware()->Container()->get('dbal_connection');
+        $eventManager = Shopware()->Container()->get('events');
 
-        $this->SUT = new DbalHelper($connection, $modelManagerStub);
+        $this->SUT = new DbalHelper($connection, $modelManagerStub, $eventManager);
     }
 
     protected function tearDown()
@@ -62,12 +65,12 @@ class DbalHelperTest extends \PHPUnit_Framework_TestCase
         $expectedSQL = 'INSERT INTO example_table (`name_in_db_one`, `name_in_db_two`) VALUES(:dcValue1, :dcValue2)';
         $importData = [
             self::MODEL_FIELD_NAME => self::IMPORT_VALUE,
-            self::ANOTHER_MODEL_FIELD_NAME => self::ANOTHER_IMPORT_VALUE
+            self::ANOTHER_MODEL_FIELD_NAME => self::ANOTHER_IMPORT_VALUE,
         ];
 
-        $builder = $this->SUT->getQueryBuilderForEntity($importData, 'Shopware\Models\Article\Article', self::CREATE_INSERT_STATEMENT);
+        $builder = $this->SUT->getQueryBuilderForEntity($importData, Article::class, self::CREATE_INSERT_STATEMENT);
 
-        $this->assertEquals($expectedSQL, $builder->getSQL(), "Could not generate insert sql statement.");
+        $this->assertEquals($expectedSQL, $builder->getSQL(), 'Could not generate insert sql statement.');
     }
 
     public function test_get_query_builder_for_entity_should_return_update_query()
@@ -81,10 +84,10 @@ class DbalHelperTest extends \PHPUnit_Framework_TestCase
 
         $givenData = [
             self::MODEL_FIELD_NAME => self::IMPORT_VALUE,
-            self::ANOTHER_MODEL_FIELD_NAME => self::ANOTHER_IMPORT_VALUE
+            self::ANOTHER_MODEL_FIELD_NAME => self::ANOTHER_IMPORT_VALUE,
         ];
 
-        $builder = $this->SUT->getQueryBuilderForEntity($givenData, 'Shopware\Models\Article\Article', self::UPDATE_RECORD_BY_ID);
+        $builder = $this->SUT->getQueryBuilderForEntity($givenData, Article::class, self::UPDATE_RECORD_BY_ID);
 
         $this->assertEquals($expectedSQL, $builder->getSQL(), "SQL UPDATE statement wasn't generated correctly");
         $this->assertEquals($expectedSQLParams, $builder->getParameters(), "Parameters for query builder wasn't set correctly.");
@@ -113,20 +116,20 @@ class DbalHelperTest extends \PHPUnit_Framework_TestCase
         $classMetaData = new ClassMetadata('ExampleEntity', null);
 
         $classMetaData->table = [
-            'name' => 'example_table'
+            'name' => 'example_table',
         ];
 
         $classMetaData->fieldMappings = [
             self::MODEL_FIELD_NAME => [
                 'fieldName' => self::MODEL_FIELD_NAME,
                 'columnName' => self::DB_COLUMN_NAME,
-                'type' => 'string'
+                'type' => 'string',
             ],
             self::ANOTHER_MODEL_FIELD_NAME => [
                 'fieldName' => self::ANOTHER_MODEL_FIELD_NAME,
                 'columnName' => self::ANOTHER_DB_COLUMN_NAME,
-                'type' => 'string'
-            ]
+                'type' => 'string',
+            ],
         ];
 
         return $classMetaData;
