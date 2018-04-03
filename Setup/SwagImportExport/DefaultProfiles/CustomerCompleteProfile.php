c46c8abd5aaaa283335137ad5fe4247487831f49
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-04-03 16:51:51 +0200

    PT-9051 - Fix customer complete export

--- Setup/SwagImportExport/DefaultProfiles/CustomerCompleteProfile.php
+++ Setup/SwagImportExport/DefaultProfiles/CustomerCompleteProfile.php
@@ -57,9 +57,9 @@ class CustomerCompleteProfile implements \JsonSerializable, ProfileMetaData
                             'name' => 'HeaderChild',
                             'index' => 0,
                             'type' => 'node',
-                            'shopwareField' => ''
-                        ]
-                    ]
+                            'shopwareField' => '',
+                        ],
+                    ],
                 ],
                 1 => [
                     'id' => '537359399c8b7',
@@ -76,11 +76,11 @@ class CustomerCompleteProfile implements \JsonSerializable, ProfileMetaData
                             'adapter' => 'customers',
                             'parentKey' => '',
                             'shopwareField' => '',
-                            'children' => $this->getCustomerFields()
-                        ]
-                    ]
-                ]
-            ]
+                            'children' => $this->getCustomerFields(),
+                        ],
+                    ],
+                ],
+            ],
         ];
     }
 
@@ -95,175 +95,175 @@ class CustomerCompleteProfile implements \JsonSerializable, ProfileMetaData
                 'type' => 'leaf',
                 'index' => 0,
                 'name' => 'customerNumber',
-                'shopwareField' => 'number'
+                'shopwareField' => 'number',
             ],
             1 => [
                 'id' => '53ea052c8f4c9',
                 'type' => 'leaf',
                 'index' => 1,
                 'name' => 'email',
-                'shopwareField' => 'email'
+                'shopwareField' => 'email',
             ],
             2 => [
                 'id' => '53ea0535e3348',
                 'type' => 'leaf',
                 'index' => 2,
                 'name' => 'password',
-                'shopwareField' => 'hashPassword'
+                'shopwareField' => 'hashPassword',
             ],
             3 => [
                 'id' => '53fb366466188',
                 'type' => 'leaf',
                 'index' => 3,
                 'name' => 'encoder',
-                'shopwareField' => 'encoderName'
+                'shopwareField' => 'encoderName',
             ],
             4 => [
                 'id' => '540d9e8c6ab4f',
                 'type' => 'leaf',
                 'index' => 4,
                 'name' => 'active',
-                'shopwareField' => 'active'
+                'shopwareField' => 'active',
             ],
             5 => [
                 'id' => '53ea054339f8e',
                 'type' => 'leaf',
                 'index' => 5,
                 'name' => 'accountMode',
-                'shopwareField' => 'accountMode'
+                'shopwareField' => 'accountMode',
             ],
             6 => [
                 'id' => '53ea057725a7d',
                 'type' => 'leaf',
                 'index' => 6,
                 'name' => 'confirmationKey',
-                'shopwareField' => 'confirmationKey'
+                'shopwareField' => 'confirmationKey',
             ],
             7 => [
                 'id' => '53ea0595b1d31',
                 'type' => 'leaf',
                 'index' => 7,
                 'name' => 'paymentId',
-                'shopwareField' => 'paymentId'
+                'shopwareField' => 'paymentId',
             ],
             8 => [
                 'id' => '53ea05dba6a4d',
                 'type' => 'leaf',
                 'index' => 8,
                 'name' => 'firstLogin',
-                'shopwareField' => 'firstLogin'
+                'shopwareField' => 'firstLogin',
             ],
             9 => [
                 'id' => '53ea05de1204b',
                 'type' => 'leaf',
                 'index' => 9,
                 'name' => 'lastLogin',
-                'shopwareField' => 'lastLogin'
+                'shopwareField' => 'lastLogin',
             ],
             10 => [
                 'id' => '53ea05df9caf1',
                 'type' => 'leaf',
                 'index' => 10,
                 'name' => 'sessionId',
-                'shopwareField' => 'sessionId'
+                'shopwareField' => 'sessionId',
             ],
             11 => [
                 'id' => '53ea05e271edd',
                 'type' => 'leaf',
                 'index' => 11,
                 'name' => 'newsletter',
-                'shopwareField' => 'newsletter'
+                'shopwareField' => 'newsletter',
             ],
             12 => [
                 'id' => '53ea05e417656',
                 'type' => 'leaf',
                 'index' => 12,
                 'name' => 'validation',
-                'shopwareField' => 'validation'
+                'shopwareField' => 'validation',
             ],
             13 => [
                 'id' => '53ea05e5e2e12',
                 'type' => 'leaf',
                 'index' => 13,
                 'name' => 'affiliate',
-                'shopwareField' => 'affiliate'
+                'shopwareField' => 'affiliate',
             ],
             14 => [
                 'id' => '53ea065093393',
                 'type' => 'leaf',
                 'index' => 14,
                 'name' => 'customerGroupKey',
-                'shopwareField' => 'groupKey'
+                'shopwareField' => 'groupKey',
             ],
             15 => [
                 'id' => '53ea0652597f1',
                 'type' => 'leaf',
                 'index' => 15,
                 'name' => 'paymentPresetId',
-                'shopwareField' => 'paymentPreset'
+                'shopwareField' => 'paymentPreset',
             ],
             16 => [
                 'id' => '53ea0653ddf4a',
                 'type' => 'leaf',
                 'index' => 16,
                 'name' => 'languageId',
-                'shopwareField' => 'languageId'
+                'shopwareField' => 'languageId',
             ],
             17 => [
                 'id' => '53ea0691b1774',
                 'type' => 'leaf',
                 'index' => 17,
                 'name' => 'referer',
-                'shopwareField' => 'referer'
+                'shopwareField' => 'referer',
             ],
             18 => [
                 'id' => '53ea069d37da6',
                 'type' => 'leaf',
                 'index' => 18,
                 'name' => 'failedLogins',
-                'shopwareField' => 'failedLogins'
+                'shopwareField' => 'failedLogins',
             ],
             19 => [
                 'id' => '53ea069eac2c6',
                 'type' => 'leaf',
                 'index' => 19,
                 'name' => 'lockedUntil',
-                'shopwareField' => 'lockedUntil'
+                'shopwareField' => 'lockedUntil',
             ],
             20 => [
                 'id' => '53ea06a0013c7',
                 'type' => 'leaf',
                 'index' => 20,
                 'name' => 'title',
-                'shopwareField' => 'title'
+                'shopwareField' => 'title',
             ],
             21 => [
                 'id' => '53ea06a23cdc1',
                 'type' => 'leaf',
                 'index' => 21,
                 'name' => 'firstname',
-                'shopwareField' => 'firstname'
+                'shopwareField' => 'firstname',
             ],
             22 => [
                 'id' => '53ea0e4a3792d',
                 'type' => 'leaf',
                 'index' => 22,
                 'name' => 'lastname',
-                'shopwareField' => 'lastname'
+                'shopwareField' => 'lastname',
             ],
             23 => [
                 'id' => '53ea0e4fda6e7',
                 'type' => 'leaf',
                 'index' => 23,
                 'name' => 'birthday',
-                'shopwareField' => 'birthday'
+                'shopwareField' => 'birthday',
             ],
             24 => [
                 'id' => '53ea0e55b2b31',
                 'type' => 'leaf',
                 'index' => 24,
                 'name' => 'priceGroupId',
-                'shopwareField' => 'priceGroupId'
+                'shopwareField' => 'priceGroupId',
             ],
             25 => [
                 'id' => '55dc3386b8c5f',
@@ -276,9 +276,9 @@ class CustomerCompleteProfile implements \JsonSerializable, ProfileMetaData
                         'name' => 'address',
                         'index' => 0,
                         'type' => 'raw',
-                        'rawKey' => 'addresses'
-                    ]
-                ]
+                        'rawKey' => 'addresses',
+                    ],
+                ],
             ],
             26 => [
                 'id' => '537359399c8b8',
@@ -304,16 +304,23 @@ class CustomerCompleteProfile implements \JsonSerializable, ProfileMetaData
                                         'name' => 'detail',
                                         'index' => 0,
                                         'type' => 'raw',
-                                        'rawKey' => 'details'
-                                    ]
+                                        'rawKey' => 'details',
+                                    ],
                                 ],
-                                'shopwareField' => ''
-                            ]
-                        ]
-                    ]
+                                'shopwareField' => '',
+                            ],
+                        ],
+                    ],
                 ],
-                'shopwareField' => ''
-            ]
+                'shopwareField' => '',
+            ],
+            27 => [
+                'id' => '586f6b33eed94',
+                'name' => 'attribute',
+                'index' => 27,
+                'type' => 'leaf',
+                'shopwareField' => 'attribute',
+            ],
         ];
     }
 }
