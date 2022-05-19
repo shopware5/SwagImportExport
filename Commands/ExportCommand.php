a18d345da251227b6d8ad10c015252543cdce486
Pascal Thesing <pascal@thesing.me>
2022-05-19 11:16:47 +0200

    PT-12610 - adjust command namespace

--- Commands/ExportCommand.php
+++ Commands/ExportCommand.php
@@ -6,7 +6,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Shopware\Commands\SwagImportExport;
+namespace SwagImportExport\Commands;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Shopware\Commands\ShopwareCommand;
