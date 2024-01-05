5475dcc0d748900b688b12fe7b7bcc0ba86901d5
Michael Telgmann <mt@shopware.com>
2024-01-05 13:40:23 +0100

    chore: Prepare release

--- Tests/Helper/CommandTestCaseTrait.php
+++ Tests/Helper/CommandTestCaseTrait.php
@@ -61,6 +61,6 @@ trait CommandTestCaseTrait
 
     private function getFilePath(string $fileName): string
     {
-        return sprintf('%s/%s', $this->getContainer()->getParameter('kernel.project_dir'), $fileName);
+        return sprintf('%s%s%s', $this->getContainer()->getParameter('kernel.project_dir'), \DIRECTORY_SEPARATOR, $fileName);
     }
 }
